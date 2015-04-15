bcbb681c66eafb3a5525f6c5ade63684d4fd6e5d
collet_k <kevin.colleter@epitech.eu>
2015-04-15 19:14:37 +0200

    Amelioration des fichiers Parser.cpp Parser.hpp

--- Parser.cpp
+++ Parser.cpp
@@ -5,7 +5,7 @@
 // Login   <collet_k@epitech.net>
 //
 // Started on  Tue Apr 14 11:49:29 2015 Kévin Colléter
-// Last update Wed Apr 15 10:50:24 2015 Kévin Colléter
+// Last update Wed Apr 15 19:10:57 2015 Kévin Colléter
 //
 
 #include "Parser.hpp"
@@ -17,6 +17,17 @@ Parser::Parser(int ac, char **av)
   this->multi = 0;
   this->cooks = 0;
   this->time = 0;
+
+  this->type[0] = "Regina";
+  this->type[1] = "Margarita";
+  this->type[2] = "Americaine";
+  this->type[3] = "Fantasia";
+
+  this->size[0] = "S";
+  this->size[1] = "M";
+  this->size[2] = "L";
+  this->size[3] = "XL";
+  this->size[4] = "XXL";
 }
 
 Parser::~Parser()
@@ -75,24 +86,105 @@ int	Parser::Read_Standard(void)
       std::getline(std::cin, cmd);
       while ((pos = cmd.find(delimiter)) != std::string::npos) {
 	token = cmd.substr(0, pos);
-	command.push_back(token);
+	Check_Command(token);
 	cmd.erase(0, pos + 1);
       }
-      command.push_back(cmd);
+      Check_Command(cmd);
       cmd.clear();
-      Check_Command();
     }
   return (GOOD);
 }
 
-int	Parser::Check_Command(void)
+int	Parser::Check_Command(std::string &token)
 {
-  Display();
+  std::string	param1;
+  std::string	param2;
+  std::string	param3;
+  std::string	delimiter = " ";
+
+  if (Nbr_Word(token) != 3) {
+    std::cerr << "[" << token << "]";
+    return (Print_Error(" : Error <Which Type> <Which Size> <How Many>"));
+  }
+  param1 = Set_Param(token);
+  param2 = Set_Param(token);
+  param3 = Set_Param(token);
+  if ((Check_Param1(param1) == ERROR) ||
+      (Check_Param2(param2) == ERROR) ||
+      (Check_Param3(param3) == ERROR))
+    return (ERROR);
+  this->command.push_back(param1 + " " + param2 + " " + param3);
   return (GOOD);
+}
+
+std::string	Parser::Set_Param(std::string &token)
+{
+  int		i = 0;
+  std::string	ret;
+
+  while ((token[i] == ' ' || token[i] == '\t') && token[i] != '\0')
+    ++i;
+  while (token[i] != ' ' && token[i] != '\t' && token[i] != '\0')
+    ret += token[i++];
+  token.erase(0, i);
+  return (ret);
+}
+
+int	Parser::Check_Param1(std::string &token)
+{
+  for (int i = 0; i < 4; i++)
+    if (token.compare(this->type[i]) == 0)
+      return (GOOD);
+  std::cerr << "Type : [" << token;
+  return (Print_Error("] don't find"));
+}
+
+int	Parser::Check_Param2(std::string &token)
+{
+  for (int i = 0; i < 5; i++)
+    if (token.compare(this->size[i]) == 0)
+      return (GOOD);
+  std::cerr << "Syze : [" << token;
+  return (Print_Error("] don't find"));
+}
+
+int	Parser::Check_Param3(std::string &token)
+{
+  if (token[0] != 'x')
+    {
+      std::cerr << "Bad usage [" << token << "]\nUsage : ";
+      return (Print_Error("x[numbers]"));
+    }
+  for (unsigned int i = 1; i != token.size(); i++)
+    if (token[i] < '0' || token[i] > '9')
+      {
+	std::cerr << "Bad usage [" << token << "]\nUsage : ";
+	return (Print_Error("x[numbers]"));
+      }
+  return (GOOD);
+}
+
+int	Parser::Nbr_Word(std::string &token)
+{
+  int	i = 0;
+  int	count = 0;
+
+  while (token[i] != 0)
+    {
+      while ((token[i] == ' ' || token[i] == '\t') && token[i] != '\0')
+	++i;
+      if (token[i] == 0)
+	return (count);
+      while (token[i] != ' ' && token[i] != '\t' && token[i] != '\0')
+	++i;
+      ++count;
+    }
+  return (count);
 }
 
 void	Parser::Display(void) const
 {
+  std::cout << "\033[1;31mEn cours :\033[00m" << std::endl;
   for (unsigned int i = 0; i < command.size(); i++)
     std::cout << "- " << command[i] << std::endl;
 }
--- Parser.hpp
+++ Parser.hpp
@@ -5,7 +5,7 @@
 // Login   <collet_k@epitech.net>
 // 
 // Started on  Tue Apr 14 11:49:21 2015 Kévin Colléter
-// Last update Wed Apr 15 10:41:06 2015 Kévin Colléter
+// Last update Wed Apr 15 18:53:48 2015 Kévin Colléter
 //
 
 #ifndef PARSER_HPP_
@@ -31,8 +31,13 @@ public:
   int	Is_Number(char *);
   int	Set_Value(char **);
   int	Read_Standard(void);
-  int	Check_Command(void);
+  int	Check_Command(std::string &);
+  int	Nbr_Word(std::string &);
   void	Display(void) const;
+  int	Check_Param1(std::string &);
+  int	Check_Param2(std::string &);
+  int	Check_Param3(std::string &);
+  std::string	Set_Param(std::string &);
 
   /* Getter */
   int	get_argc(void) const;
@@ -43,11 +48,13 @@ public:
   std::vector<std::string> get_command(void) const;
 
 private:
-  int	argc;
   char	**argv;
+  int	argc;
   int	multi;
   int	cooks;
   int	time;
+  const char	*type[4];
+  const char	*size[5];
   std::vector<std::string> command;
 };
 
